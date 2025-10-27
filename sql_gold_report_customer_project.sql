4b05f5b1550b45905d7b6c1335b54e80d9224c6e
Oscar-Corrales <oscarcorrales02@gmail.com>
2025-10-27 09:33:55 -0500

    sql_gold_report_customer_project.sql

--- sql_gold_report_customer_project.sql
+++ sql_gold_report_customer_project.sql
@@ -1,5 +1,5 @@
 /* 
-========================================================================================
+===========================================================================================
 Customer Report 
 
 Purpose: 
@@ -128,3 +128,17 @@ case
 	else total_sales/lifespan 
 	end as avg_monthly_spend  
 from customer_aggregation
+
+/*==============================================================================
+Other analysts can pull from this report in gold.report_customers, such as to 
+analyze customer data by age group. They can count customers, sum total sales, 
+visualize trends, and generate insights about which age groups contribute most
+to overall sales, all without querying the raw table directly.
+==============================================================================*/
+
+select 
+age_group,
+count(customer_key) as total_customers ,
+sum(total_sales) as total_sales 
+from gold.report_customers 
+group by age_group
